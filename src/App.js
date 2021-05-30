34ecea6319368c3cfcae45735359770892ec86a2
Tony Kharioki <antolisah@gmail.com>
2021-05-30 05:34:19 +0300

    fetching data and loader component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
+import Loader from './components/Loader';
 import Map from "./components/Map";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=VusTovpfG5969YimsViziwYwJ2o9G2Vv6lmY3faf');
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <div className="App">
-      <Map />
+      {!loading ? <Map eventData={eventData} /> : <Loader /> }
     </div>
   );
 }
