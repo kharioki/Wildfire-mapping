1b2a9a5887033367a94bfdc90da7106c64a60dee
Tony Kharioki <antolisah@gmail.com>
2021-05-30 06:29:41 +0300

    header and location info box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Header from './components/Header';
 import Loader from './components/Loader';
 import Map from "./components/Map";
 
@@ -20,6 +21,7 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <Header />
       {!loading ? <Map eventData={eventData} /> : <Loader /> }
     </div>
   );
